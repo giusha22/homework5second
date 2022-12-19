2ee4be968bf7f98fc2d5449e82511e88c0b3fa8d
Giorgi Mtsituri <giorgimtsituri42@gmail.com>
2022-12-19 18:11:49 +0400

    homework 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {users: [] };
+
+  }
+    onAddUser = ()=>{
+    const newUser = {
+      name: "giorgi",
+      age: Math.floor(Math.random() * (60 - 10 + 1) + 10),
+      id: new Date().toString(),
+    };
+    this.setState((prevState)=> ({
+      users:[...prevState.users, newUser]
+    }) )
+  } 
+  onDeleteUser = (id)=>{
+    console.log("id",id);
+    this.setState((prevState)=>({
+      users: prevState.users.filter((user)=> user.id  !== id)
+    }));
+  };
+  onUpdateUser = (id)=>{
+    this.setState((prevState)=>{
+        const newUsersList = prevState.users.reduce((acc,current)=>{
+          if(current.id === id){
+            current.age = 50;
+            current.name = "vaskaa";
+            }
+            return [...acc, current]
+        },[]);
+        return {
+          users: newUsersList
+        };
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onAddUser}>Add user</button>
+        {this.state.users.map((user)=>{
+          return <div key={user.id} style={{display: "flex", justifyContent: "space-between"}}>
+            <h1>{user.name}</h1>
+            <h2>{user.age } </h2>
+            <button onClick={ ()=> this.onUpdateUser(user.id)}>Update</button>
+            <button onClick={()=> this.onDeleteUser(user.id)}>Delete</button>
+
+
+          </div>
+        })}
+      </div>
+    )
+  }
 }
-
-export default App;
+ 
